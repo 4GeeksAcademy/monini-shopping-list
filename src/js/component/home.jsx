b317b2b02e0964441d38d19e2980a96dff6cb2f7
Monique Latil <moninilatil@gmail.com>
2024-11-16 16:37:28 +0000

    no sé cómo siga siendo visible la línea inferior al agregar un elemento

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,72 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [shopping, setShopping] = useState([]);
+  const [hoveredTask, setHoveredTask] = useState(null);
+
+  const deleteItem = (id) => {
+    setShopping(shopping.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className="content">
+      <div className="tittle">
+        <h1>
+          Shopping list{" "}
+          <i className="fa-solid fa-basket-shopping" style={{ color: "purple" }}></i>
+        </h1>
+      </div>
+      <div className="container">
+        <div>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && inputValue.trim() !== "") {
+                setShopping([
+                  ...shopping,
+                  { id: Math.random(), text: inputValue },
+                ]);
+                setInputValue("");
+              }
+            }}
+            placeholder="What do you need to buy?"
+          />
+        </div>
+
+        <ul>
+          {shopping.length === 0 ? ( // mensaje si no hay tareas
+            <li>There's nothing in the shopping list, add items</li>
+          ) : (
+            shopping.map((item) => (
+              <li
+                key={item.id}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  padding: "5px 0",
+                  borderBottom: "1px solid #ddd",
+                }}
+                onMouseEnter={() => setHoveredTask(item.id)} // Muestra el ícono al pasar el ratón
+                onMouseLeave={() => setHoveredTask(null)} // Oculta el ícono al salir
+              >
+                {item.text}
+                  <i
+                    className="fa-solid fa-x"
+                    onClick={() => deleteItem(item.id)}
+                  ></i>
+                
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
